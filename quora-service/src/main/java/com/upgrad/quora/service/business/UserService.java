fe2c7b4ed3b94e84d058847e337b40d900368423
chaitraw <chaitra.warad@gmail.com>
2020-02-15 21:10:25 +0530

    Added createUser in UserService class

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserService.java
@@ -0,0 +1,35 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Service class which deals with business logic related to User entity
+ *
+ * @author chaitraw
+ */
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private PasswordCryptographyProvider cryptographyProvider;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+
+
+    public UserEntity createUser(UserEntity userEntity) {
+        String password = userEntity.getPassword();
+        if (password == null) {
+            userEntity.setPassword("quora@123");
+        }
+        String[] encryptedText = cryptographyProvider.encrypt(userEntity.getPassword());
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+        return userDao.createUser(userEntity);
+    }
+}
